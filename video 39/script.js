e7ea3b6695dad5375d63bef1c7ec193d89256b95
viveklohar229 <117832154+viveklohar229@users.noreply.github.com>
2024-09-14 11:38:47 +0530

    video 39
    JavaScript Loops

---
+++ video 39/script.js
@@ -0,0 +1,43 @@
+console.log("I am a tutorial on Loops");
+
+let a = 1;
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+// for (let i = 0; i < 100; i++) {
+// console.log(a + i );
+// }
+
+// let obj = {
+//     "name": "Vivek",
+//     "role": "Programming",
+//     "company": "CodeWithAV"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element);
+
+// }
+
+// for (const key in obj) {
+//     console.log(key);
+
+// }
+
+// for (const c of "VIVEK") {
+//     console.log(c);
+// }
+
+// let i = 0;
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 6;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
