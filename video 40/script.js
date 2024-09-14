05e07c3db516b3d234e9f553930b2456db083e90
viveklohar229 <117832154+viveklohar229@users.noreply.github.com>
2024-09-14 12:07:00 +0530

    video 40
    JavaScript Functions
    Arrow Function

---
+++ video 40/script.js
@@ -0,0 +1,43 @@
+// console.log("Hey Vivek You are nice!")
+// console.log("Hey Vivek You are good!")
+// console.log("Hey Vivek your t-shirt is nice!")
+// console.log("Hey Vivek your course is good too")
+
+
+// function nice(name) {
+//     console.log("Hey " + name + " You are nice!")
+//     console.log("Hey " + name + " You are good!")
+//     console.log("Hey " + name + " your t-shirt is nice!")
+//     console.log("Hey " + name + " your course is good too!")
+// }
+// nice("rohan")
+// nice("shivam")
+// function sum(a, b) {
+//     // console.log(a + b)
+//     return a + b 
+// }
+
+// result = sum( 3 , 5 ) 
+
+// function sum(a, b, c=3) {
+//     // console.log(a + b)
+//     return a + b + c
+// }
+
+// result1 = sum( 3)   // Return NaN --> Not a Number
+// result2 = sum( 7 , 5 ) 
+// result3 = sum( 13 , 5 ) 
+
+// console.log("The sum of these numbers is: ", result1)
+// console.log("The sum of these numbers is: ", result2)
+// console.log("The sum of these numbers is: ", result3)
+
+
+// Arrow Function
+const func1 = (x)=>{
+    console.log("I am an arrow function", x)
+}
+
+func1(34);
+func1(84);
+func1(134);
