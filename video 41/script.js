c17f3cc39d8ad6d5dab6a43d3792e45adbd036a4
viveklohar229 <117832154+viveklohar229@users.noreply.github.com>
2024-09-24 01:16:31 +0530

    video 41
    JavaScript String

---
+++ video 41/script.js
@@ -0,0 +1,32 @@
+console.log("This is String tutorials")
+
+let a = "Vivek";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "vivek"
+let friend = "chandan"
+console.log("His name is " + real_name + " and his friend name is " + friend)
+
+console.log(`His name is ${real_name} and his friend name is ${friend}`)
+
+let b = "amitsh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(1,4))
+console.log(b.slice(1))
+
+console.log(b.replace("sh", "77"))
+console.log(b.concat(a, "chandan"))
+
+console.log(b)
+console.log(b.indexOf("a"))
+console.log(b.startsWith("am"))
+console.log(b.endsWith("sh"))
