bed6ece2260a60034450879ce07709e9da3793a2
viveklohar229 <117832154+viveklohar229@users.noreply.github.com>
2024-10-02 19:14:08 +0530

    video 44
    JavaScript DOM - Children , Parent and Sibiling Nodes

---
+++ video 44/script.js
@@ -0,0 +1,35 @@
+console.log("Hello World")
+
+document.body.childNodes[1]
+
+document.body.childNodes[1].childNodes
+
+document.body.childNodes[1].childNodes[1]
+
+document.body.childNodes[1].childNodes[1].childNodes
+
+let cont = document.body.childNodes[1]
+cont
+cont.firstChild
+cont.firstElementChildChild
+cont.lastElementChild
+cont.lastElementChild
+cont.lastElementChild.style.color = "red";
+cont.lastElementChild.style.backgroundColor = "green";
+cont.lastElementChild.parentElement
+cont.lastElementChild.parentElement.style.backgroundColor = "blue";
+
+
+document.body.firstElementChild
+
+document.body.firstElementChild.childNodes
+
+document.body.firstElementChild.children
+
+
+
+document.body.children
+
+document.body.children[1]
+
+document.body.children[1].rows
